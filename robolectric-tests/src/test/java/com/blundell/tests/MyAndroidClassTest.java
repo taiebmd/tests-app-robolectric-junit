e186d5083ce71ade5af0e0cec0770b75f9787026
taiebmd <taieb.md@gmail.com>
2015-05-05 03:54:35 +0200

    Update MyAndroidClassTest.java

--- robolectric-tests/src/test/java/com/blundell/tests/MyAndroidClassTest.java
+++ robolectric-tests/src/test/java/com/blundell/tests/MyAndroidClassTest.java
@@ -9,16 +9,16 @@ import org.robolectric.annotation.Config;
 @Config(emulateSdk = 18, reportSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class MyAndroidClassTest {
-
+    /*
     @Test
     public void fakeTest() {
-        /*MyActivity activity = new MyActivity();
+        //MyActivity activity = new MyActivity();
 
-        ActivityController.of(activity).attach().create();
+        //ActivityController.of(activity).attach().create();
 
-        int visibility = activity.findViewById(R.id.my_hello_text_view).getVisibility();*/
+        //int visibility = activity.findViewById(R.id.my_hello_text_view).getVisibility();
         Assert.assertFalse(false);
-        /*Assert.assertNotNull(activity);
-        assertEquals(visibility, View.VISIBLE);*/
-    }
+        //Assert.assertNotNull(activity);
+       // assertEquals(visibility, View.VISIBLE);
+    }*/
 }
